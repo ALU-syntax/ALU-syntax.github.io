4d10820b53c41b85f50986c996bd08220396cee8
AdlyRadwika <85500151+AdlyRadwika@users.noreply.github.com>
2022-06-09 21:36:39 +0700

    for isas againagain

---
+++ captcha.php
@@ -0,0 +1,34 @@
+
+        $email;$comment;$captcha;
+        if(isset($_POST['email'])){
+          $email=$_POST['email'];
+        }
+        if(isset($_POST['comment'])){
+          $comment=$_POST['comment'];
+        }
+        if(isset($_POST['g-recaptcha-response'])){
+          $captcha=$_POST['g-recaptcha-response'];
+        }
+        if(!$captcha){
+          echo '
+Please check the the captcha form.
+';
+          exit;
+        }
+        $secretKey = "Put your secret key here";
+        $ip = $_SERVER['REMOTE_ADDR'];
+        // post request to server
+        $url = 'https://www.google.com/recaptcha/api/siteverify?secret=' . urlencode($secretKey) .  '&response=' . urlencode($captcha);
+        $response = file_get_contents($url);
+        $responseKeys = json_decode($response,true);
+        // should return JSON with success as true
+        if($responseKeys["success"]) {
+                echo '
+Thanks for posting comment
+';
+        } else {
+                echo '
+You are spammer ! Get the @$%K out
+';
+        }
+?>
